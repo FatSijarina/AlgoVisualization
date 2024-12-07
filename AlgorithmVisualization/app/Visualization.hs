7a17418eaa611961edc58a0c62669b7b2e75e9bf
BleonaGerbavci <bg52732@ubt-uni.net>
2024-12-07 21:09:09 +0100

    made the visualizations slower

--- AlgorithmVisualization/app/Visualization.hs
+++ AlgorithmVisualization/app/Visualization.hs
@@ -4,62 +4,51 @@ import Graphics.Gloss
 import Algorithms (SortStep(..))
 import Data.Maybe (catMaybes)
 
--- Function to draw the state of the algorithm at each step
 drawState :: String -> [Int] -> [Int] -> (Maybe Int, Maybe Int) -> Picture
 drawState title xs activeIndices (i, j) = Pictures [barsPic, titlePic, indicesPic]
   where
-    -- Dimensions and scaling
     barWidth = 40.0 :: Float
-    spacing = 10.0 :: Float
+    spacing = 20.0 :: Float
     scaleFactor = 20.0 :: Float
     totalWidth = fromIntegral (length xs) * (barWidth + spacing) - spacing
     maxBarHeight = fromIntegral (maximum xs) * scaleFactor
 
-    -- Centering offsets
     xOffset = -totalWidth / 2
     yOffset = -maxBarHeight / 2
 
-    -- Color logic for bars
     barColor idx = if idx `elem` activeIndices then red else light blue
 
-    -- Draw the bars and their labels
     barsPic = Pictures $ map drawBar (zip [0..] xs)
     drawBar (idx, val) = Pictures [barPic, labelPic]
       where
         xPos = fromIntegral idx * (barWidth + spacing) + xOffset
         barHeight = fromIntegral val * scaleFactor
         barPic = translate xPos (yOffset + barHeight / 2) $
-                 color (barColor idx) $ rectangleSolid barWidth barHeight
-        labelPic = translate xPos (yOffset - 15) $
-                   scale 0.15 0.15 $ color black $ Text (show val)
+                color (barColor idx) $ rectangleSolid barWidth barHeight
+        -- Përllogaritje e përafërt e gjysmës së gjatësisë së tekstit
+        textWidth = fromIntegral (length (show val)) * 5.0
+        labelPic = translate (xPos - textWidth / 2) (yOffset - 30) $
+                  scale 0.15 0.15 $ color black $ Text (show val)
 
-    -- Calculate width of the title dynamically
-    titleWidth = fromIntegral (length title) * 12.0  -- Rough estimate of text width per character
-
-    -- Title at the top, centered
+    titleWidth = fromIntegral (length title) * 12.0
     titlePic = translate (-titleWidth / 2) (maxBarHeight / 2 + 50) $
                scale 0.2 0.2 $ color black $ Text title
 
-    -- Draw the indices i and j
     indicesPic = Pictures $ catMaybes [drawIndex i "i", drawIndex j "j"]
-
-    -- Function to draw an index label (i or j) if the index is Just
     drawIndex (Just idx) label =
       let xPos = fromIntegral idx * (barWidth + spacing) + xOffset
       in Just $ translate xPos (yOffset + maxBarHeight + 20) $
                 scale 0.15 0.15 $ color green $ Text label
-    drawIndex Nothing _ = Nothing  -- Don't draw if index is Nothing
+    drawIndex Nothing _ = Nothing
 
--- Function to animate the states
 animateStates :: String -> [SortStep Int] -> IO ()
-animateStates title steps = simulate window white fps initialIndex render update
+animateStates title steps = animate window white frameRenderer
   where
     window = InWindow "Algorithm Visualization" (800, 600) (100, 100)
-    fps = 1
-    initialIndex = 0
+    delayPerFrame = 3.0
 
-    render idx =
-      let SortStep lst active current = steps !! idx
+    frameRenderer :: Float -> Picture
+    frameRenderer time =
+      let idx = min (floor (time / delayPerFrame)) (length steps - 1)
+          SortStep lst active current = steps !! idx
       in drawState title lst active current
-
-    update _ _ idx = if idx < length steps - 1 then idx + 1 else idx
--- AlgorithmVisualization/app/algorithms/BubbleSort.hs
+++ AlgorithmVisualization/app/algorithms/BubbleSort.hs
@@ -11,15 +11,12 @@ bubbleSortSteps xs = go xs []
           let (newList, active, i, j) = bubble lst 0
           in go newList (SortStep lst active (Just i, Just j) : acc)
 
-    -- Bubble the elements, keeping track of indices being compared
-    bubble [x] _ = ([x], [], -1, -1)  -- Base case, nothing to compare
+    bubble [x] _ = ([x], [], -1, -1)
     bubble (x : y : rest) idx
       | x > y     = let (bubbled, active, i, j) = bubble (x : rest) (idx + 1)
-                    in (y : bubbled, [idx, idx + 1], idx, idx + 1)  -- Track the current and next indices
+                    in (y : bubbled, [idx, idx + 1], idx, idx + 1)
       | otherwise = let (bubbled, active, i, j) = bubble (y : rest) (idx + 1)
-                    in (x : bubbled, [idx + 1], idx + 1, idx + 2)  -- Continue with the rest
-
-    -- Check if the list is sorted
+                    in (x : bubbled, [idx + 1], idx + 1, idx + 2)
     isSorted [] = True
     isSorted [x] = True
     isSorted (x : y : rest) = x <= y && isSorted (y : rest)
