bcb6a42bb086f982a008ee616dd5312a8dbf32dc
FatSijarina <fs51701@ubt-uni.net>
2024-12-17 16:58:11 +0100

    enhanced MergeSort and BubbleSort visualization

--- AlgorithmVisualization/app/algorithms/BubbleSort.hs
+++ AlgorithmVisualization/app/algorithms/BubbleSort.hs
@@ -7,28 +7,62 @@ bubbleSortSteps xs = go xs 0 []
   where
     go lst pass acc
       | isSorted lst =
+          -- Final step: entire array is sorted
           reverse (SortStep { listState = lst
                             , activeIndices = []
                             , currentIndices = (Nothing, Nothing)
                             , sortedIndices = [0..length lst - 1]} : acc)
       | otherwise =
-          let (newList, active, i, j) = bubble lst 0
+          let (newList, newSteps) = bubblePass lst 0 (length lst - pass - 1) []
               n = length newList
-              -- After pass-th iteration, last (pass+1) elements are sorted
               sortedSoFar = [n - (pass + 1) .. n - 1]
-          in go newList (pass + 1) (SortStep { listState = newList
-                                             , activeIndices = active
-                                             , currentIndices = (Just i, Just j)
-                                             , sortedIndices = sortedSoFar} : acc)
+              -- Add a step after the pass to show which part is now sorted
+              endStep = SortStep { listState = newList
+                                 , activeIndices = []
+                                 , currentIndices = (Nothing, Nothing)
+                                 , sortedIndices = sortedSoFar }
+          in go newList (pass + 1) (endStep : newSteps ++ acc)
 
-    bubble [x] _ = ([x], [], -1, -1)
-    bubble (x:y:rest) idx
-      | x > y =
-          let (bubbled, _, i, j) = bubble (x:rest) (idx+1)
-          in (y:bubbled, [idx, idx+1], idx, idx+1)
+    -- Perform one pass of bubble sort from index 0 to endIndex
+    -- During this pass, compare adjacent pairs and produce steps for comparisons and swaps
+    bubblePass :: (Ord a) => [a] -> Int -> Int -> [SortStep a] -> ([a], [SortStep a])
+    bubblePass lst start end steps
+      | start >= end = (lst, steps)
       | otherwise =
-          let (bubbled, _, i, j) = bubble (y:rest) (idx+1)
-          in (x:bubbled, [idx+1], idx+1, idx+2)
+          let i = start
+              j = start + 1
+              x = lst !! i
+              y = lst !! j
+              -- Comparison step
+              stepCompare = SortStep { listState = lst
+                                     , activeIndices = [i, j]
+                                     , currentIndices = (Just i, Just j)
+                                     , sortedIndices = [] }
+          in if x > y
+             then
+               -- Need to swap
+               let swapped = swap lst i j
+                   stepSwap = SortStep { listState = swapped
+                                       , activeIndices = [i, j]
+                                       , currentIndices = (Just i, Just j)
+                                       , sortedIndices = [] }
+               in bubblePass swapped (start+1) end (stepSwap : stepCompare : steps)
+             else
+               -- No swap needed
+               bubblePass lst (start+1) end (stepCompare : steps)
+
+    swap :: [a] -> Int -> Int -> [a]
+    swap arr i j
+      | i == j = arr
+      | otherwise =
+          let xi = arr !! i
+              xj = arr !! j
+          in replace j xi (replace i xj arr)
+
+    replace :: Int -> a -> [a] -> [a]
+    replace idx val xs =
+      let (before, _:after) = splitAt idx xs
+      in before ++ (val : after)
 
     isSorted [] = True
     isSorted [_] = True
--- AlgorithmVisualization/app/algorithms/MergeSort.hs
+++ AlgorithmVisualization/app/algorithms/MergeSort.hs
@@ -6,10 +6,11 @@ mergeSortSteps :: (Ord a) => [a] -> [SortStep a]
 mergeSortSteps xs = mergeSort xs 0 (length xs - 1)
   where
     mergeSort lst left right
-      | left >= right = [SortStep { listState = lst
-                                  , activeIndices = []
-                                  , currentIndices = (Nothing, Nothing)
-                                  , sortedIndices = if left <= right then [left..right] else []}]
+      | left >= right =
+          [SortStep { listState = lst
+                    , activeIndices = []
+                    , currentIndices = (Nothing, Nothing)
+                    , sortedIndices = if left <= right then [left..right] else []}]
       | otherwise =
           let mid = (left + right) `div` 2
               leftSteps = mergeSort lst left mid
@@ -18,41 +19,75 @@ mergeSortSteps xs = mergeSort xs 0 (length xs - 1)
           in leftSteps ++ rightSteps ++ mergedSteps
 
     merge lst left mid right =
-      let leftPart = take (mid - left + 1) (drop left lst)
+      let leftPart  = take (mid - left + 1) (drop left lst)
           rightPart = take (right - mid) (drop (mid+1) lst)
-          mergedSteps = mergeParts leftPart rightPart left lst
-          -- After merging, [left..right] is sorted
-      in map (\s -> s { sortedIndices = [left..right] }) mergedSteps
+          (steps, finalList) = mergeParts lst left mid right leftPart rightPart
+          finalStep = SortStep { listState = finalList
+                               , activeIndices = []
+                               , currentIndices = (Nothing, Nothing)
+                               , sortedIndices = [left..right] }
+      in steps ++ [finalStep]
 
-    mergeParts [] [] _ lst = [SortStep { listState = lst
-                                       , activeIndices = []
-                                       , currentIndices = (Nothing, Nothing)
-                                       , sortedIndices = []}] 
-    mergeParts [] ys idx lst = go ys idx lst []
-    mergeParts xs [] idx lst = go xs idx lst []
-    mergeParts (x:xs) (y:ys) idx lst
-      | x <= y =
-          let updatedList = replaceElement lst idx x
-              step = SortStep { listState = updatedList
-                              , activeIndices = [idx]
-                              , currentIndices = (Just idx, Nothing)
-                              , sortedIndices = []}
-          in step : mergeParts xs (y:ys) (idx+1) updatedList
-      | otherwise =
-          let updatedList = replaceElement lst idx y
-              step = SortStep { listState = updatedList
-                              , activeIndices = [idx]
-                              , currentIndices = (Just idx, Nothing)
-                              , sortedIndices = []}
-          in step : mergeParts (x:xs) ys (idx+1) updatedList
+    mergeParts :: (Ord a) => [a] -> Int -> Int -> Int -> [a] -> [a] -> ([SortStep a], [a])
+    mergeParts original left mid right leftPart rightPart =
+      go original left leftPart rightPart []
+      where
+        go currentList currentIndex xs ys steps
+          | null xs && null ys =
+              -- Both sides empty, merging finished
+              (steps, currentList)
 
-    go [] _ lst steps = steps
-    go (z:zs) idx lst steps =
-      let updatedList = replaceElement lst idx z
-          step = SortStep { listState = updatedList
-                          , activeIndices = [idx]
-                          , currentIndices = (Just idx, Nothing)
-                          , sortedIndices = []}
-      in go zs (idx+1) updatedList (steps ++ [step])
+          | null xs =
+              -- Left empty, place remaining right side elements
+              let (newList, newSteps) = placeRemaining currentList currentIndex ys steps
+              in (newSteps, newList)
 
-    replaceElement lst idx val = take idx lst ++ [val] ++ drop (idx+1) lst
+          | null ys =
+              -- Right empty, place remaining left side elements
+              let (newList, newSteps) = placeRemaining currentList currentIndex xs steps
+              in (newSteps, newList)
+
+          -- Both xs and ys are non-empty here, so we can pattern match safely
+          | (x:xs') <- xs, (y:ys') <- ys =
+              let currentLeftIndex = left + (length leftPart - length (x:xs'))
+                  currentRightIndex = (mid + 1) + (length rightPart - length (y:ys'))
+                  stepCompare = SortStep { listState = currentList
+                                         , activeIndices = [currentLeftIndex, currentRightIndex]
+                                         , currentIndices = (Just currentLeftIndex, Just currentRightIndex)
+                                         , sortedIndices = [] }
+              in if x <= y
+                 then
+                   -- Choose x
+                   let updatedList = replaceElement currentList currentIndex x
+                       stepPlace = SortStep { listState = updatedList
+                                            , activeIndices = [currentIndex]
+                                            , currentIndices = (Just currentIndex, Nothing)
+                                            , sortedIndices = [] }
+                   in go updatedList (currentIndex + 1) xs' ys (steps ++ [stepCompare, stepPlace])
+                 else
+                   -- Choose y
+                   let updatedList = replaceElement currentList currentIndex y
+                       stepPlace = SortStep { listState = updatedList
+                                            , activeIndices = [currentIndex]
+                                            , currentIndices = (Just currentIndex, Nothing)
+                                            , sortedIndices = [] }
+                   in go updatedList (currentIndex + 1) xs ys' (steps ++ [stepCompare, stepPlace])
+
+    -- placeRemaining places leftover elements (from one half) without comparisons
+    placeRemaining :: [a] -> Int -> [a] -> [SortStep a] -> ([a], [SortStep a])
+    placeRemaining currentList currentIndex elems steps =
+      let (finalList, finalSteps, _) =
+            foldl (\(cl, st, i) val ->
+                     let updated = replaceElement cl i val
+                         stepPlace = SortStep { listState = updated
+                                              , activeIndices = [i]
+                                              , currentIndices = (Just i, Nothing)
+                                              , sortedIndices = [] }
+                     in (updated, st ++ [stepPlace], i+1))
+                  (currentList, steps, currentIndex)
+                  elems
+      in (finalList, finalSteps)
+
+    replaceElement :: [a] -> Int -> a -> [a]
+    replaceElement lst idx val =
+      take idx lst ++ [val] ++ drop (idx + 1) lst
